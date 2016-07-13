ac0076ad3f66ad6abb95c9c65b88a5906a812122
Rasmus Hald <rasmus_hald@hotmail.com>
2016-07-13 17:09:52 +0200

    Added 2 tests

--- BrickOps/AzureResourceGroup1/Scripts/AzureRGTest.ps1
+++ BrickOps/AzureResourceGroup1/Scripts/AzureRGTest.ps1
@@ -5,4 +5,18 @@
 Find-AzureRmResource -ResourceGroupNameContains "BrickOps004" | measure-object
 $NumberOfResources = Find-AzureRmResource -ResourceGroupNameContains "BrickOps004" | measure-object
 $NumberOfResources.Count 
-$NumberOfResources.Count -eq 8
+$a = $NumberOfResources.Count -eq 8
+$a 
+if ($a -like "false") 
+    {
+    Throw "Ooops!!"
+    }
+
+$NumberOfResources = Find-AzureRmResource -ResourceGroupNameContains "BrickOps004" | measure-object
+$NumberOfResources.Count 
+$a = $NumberOfResources.Count -eq 9
+$a 
+if ($a -like "false") 
+    {
+    Throw "Ooops!!"
+    }
