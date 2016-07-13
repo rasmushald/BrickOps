3fe2878cacb4ddf9b94b5a3f4aa46c02ce460338
Rasmus Hald <rasmus_hald@hotmail.com>
2016-07-13 15:56:32 +0200

    Change to Find-AzureRmResource

--- BrickOps/AzureResourceGroup1/Scripts/AzureRGTest.ps1
+++ BrickOps/AzureResourceGroup1/Scripts/AzureRGTest.ps1
@@ -2,7 +2,4 @@
 # Script.ps1
 #
 #Get-AzureRmResource
-Get-AzureRmResource -ResourceGroupName BrickOps004
-ECHO -----------
-Get-AzureRmResource -ResourceType "microsoft.web/sites"
-#Get-AzureRmResource -ResourceType "microsoft.web/sites" -ResourceGroupName "ResourceGroup11" -ResourceName "ContosoWebsite"
+Find-AzureRmResource -ResourceGroupNameContains "BrickOps004" | measure-object
