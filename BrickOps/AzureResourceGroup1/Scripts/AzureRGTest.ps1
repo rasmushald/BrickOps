6e250916d571f42b5b3df765066b489adb38f82e
Rasmus Hald <rasmus_hald@hotmail.com>
2016-07-13 16:14:58 +0200

    First test added

--- BrickOps/AzureResourceGroup1/Scripts/AzureRGTest.ps1
+++ BrickOps/AzureResourceGroup1/Scripts/AzureRGTest.ps1
@@ -3,3 +3,6 @@
 #
 #Get-AzureRmResource
 Find-AzureRmResource -ResourceGroupNameContains "BrickOps004" | measure-object
+$NumberOfResources = Find-AzureRmResource -ResourceGroupNameContains "BrickOps004" | measure-object
+$NumberOfResources.Count 
+$NumberOfResources.Count -eq 8
