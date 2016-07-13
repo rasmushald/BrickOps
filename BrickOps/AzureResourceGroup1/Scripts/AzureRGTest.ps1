1eeff68546c315bfc8d9ca75bd0ba5df36d43bda
Rasmus Hald <rasmus_hald@hotmail.com>
2016-07-13 15:45:16 +0200

    .

--- BrickOps/AzureResourceGroup1/Scripts/AzureRGTest.ps1
+++ BrickOps/AzureResourceGroup1/Scripts/AzureRGTest.ps1
@@ -1,5 +1,8 @@
 #
 # Script.ps1
 #
-Get-AzureRmResource
+#Get-AzureRmResource
+Get-AzureRmResource -ResourceGroupName BrickOps004
+ECHO -----------
+Get-AzureRmResource -ResourceType "microsoft.web/sites"
 #Get-AzureRmResource -ResourceType "microsoft.web/sites" -ResourceGroupName "ResourceGroup11" -ResourceName "ContosoWebsite"
