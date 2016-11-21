1cf6107dcc5159f2ba2e655f6d9ad79cd1a9b517
Rasmus Hald <rasmus_hald@hotmail.com>
2016-11-21 22:08:23 +0100

    Added unit tests

---
+++ BrickOps/DemoBrickOps023/Script.ps1
@@ -0,0 +1,57 @@
+#----------------------------------------------------------------------------
+#
+# Azure RG Unit Test
+#
+Param(
+    [Parameter(Mandatory = $true)]
+    [ValidatePattern("^[a-z0-9]*$")]
+    [String]$ResourceGroup              # required    Resource Group Name    
+    )
+Echo $ResourceGroup
+Echo --------
+#Get-AzureRmResource
+Find-AzureRmResource -ResourceGroupNameContains $ResourceGroup | measure-object
+
+
+#Testing number of Resources
+$NumberOfResources = Find-AzureRmResource -ResourceGroupNameContains $ResourceGroup | measure-object
+ECHO "Number Of Resources:" $NumberOfResources.Count
+$NumberOfResourcesCount = $NumberOfResources.Count -eq 2
+ECHO "Test pass:" $NumberOfResourcesCount
+if ($NumberOfResourcesCount -like "false") 
+    {
+    Throw "Ooops!!  Something is wrong with the number of Resources in the Resource Group"
+    }
+
+
+#Testing number of Websites
+$NumberOfWebapps = Find-AzureRmResource -ResourceGroupNameContains $ResourceGroup -ResourceType "microsoft.web/sites" | measure-object
+ECHO "Number Of Azure WebApps:" $NumberOfWebapps.Count
+$NumberOfWebappsCount = $NumberOfWebapps.Count -eq 1
+ECHO "Test pass:" $NumberOfWebappsCount
+if ($NumberOfWebappsCount -like "false") 
+    {
+    Throw "Ooops!!  Something is wrong with the number of Websites"
+    }
+
+
+#Testing number of HostingPlans
+$NumberOfPlans = Find-AzureRmResource -ResourceGroupNameContains $ResourceGroup -ResourceType "Microsoft.Web/serverfarms" | measure-object
+ECHO "Number Of Hosting Plans:" $NumberOfPlans.Count
+$NumberOfPlansCount = $NumberOfPlans.Count -eq 1
+ECHO "Test pass:" $NumberOfPlansCount
+if ($NumberOfPlansCount -like "false") 
+    {
+    Throw "Ooops!! Something is wrong with the number of Hosting Plans"
+    }
+
+
+#Testing name of WebApp
+$NameOfWebApp = Find-AzureRmResource -ResourceGroupNameContains $ResourceGroup -ResourceType "Microsoft.Web/sites"
+ECHO "Name of WebApp:" $NameOfWebApp.Name
+$NameOfWebApp.name -eq $ResourceGroup
+ECHO "Test pass:" $NumberOfPlansCount
+if ($NumberOfPlansCount -like "false") 
+    {
+    Throw "Ooops!! Something is wrong with the name of the WebApp"
+    }
