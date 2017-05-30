bcc992a626c130664faaf0601bb0cfbff34be0c2
Rasmus Hald <rasmus_hald@hotmail.com>
2017-05-30 22:05:38 +0200

    Update AzureRGTest.ps1
    Fixed issue with RG number

--- BrickOps/AzureResourceGroup1/Scripts/AzureRGTest.ps1
+++ BrickOps/AzureResourceGroup1/Scripts/AzureRGTest.ps1
@@ -15,7 +15,7 @@ Find-AzureRmResource -ResourceGroupNameContains $ResourceGroup | measure-object
 #Testing number of Resources
 $NumberOfResources = Find-AzureRmResource -ResourceGroupNameContains $ResourceGroup | measure-object
 ECHO "Number Of Resources:" $NumberOfResources.Count
-$NumberOfResourcesCount = $NumberOfResources.Count -eq 9
+$NumberOfResourcesCount = $NumberOfResources.Count -eq 3
 ECHO "Test pass:" $NumberOfResourcesCount
 if ($NumberOfResourcesCount -like "false") 
     {
